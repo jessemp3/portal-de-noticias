d97af50bc255db79f5efbedf77666ba0e1ac8cc5
jesse <nitchsx@gmail.com>
2024-11-11 21:02:54 -0300

    feat : end the project

--- Posts.js
+++ Posts.js
@@ -1,17 +1,19 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
- 
-var PostSchema = new Schema({
-    titulo:String,
-    imagem:String,
-    categoria:String,
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema(
+  {
+    titulo: String,
+    imagem: String,
+    categoria: String,
     conteudo: String,
-    slug:String,
-    autor:String,
-    views:Number
-} , {collection:'posts'})
+    slug: String,
+    autor: String,
+    views: { type: Number, default: 0 },
+  },
+  { collection: "posts" }
+);
 
+var Posts = mongoose.model("Posts", PostSchema);
 
-var Posts = mongoose.model("Posts" , PostSchema);
-
-module.exports = Posts
+module.exports = Posts;
